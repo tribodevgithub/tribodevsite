0fc9e64dbee3dd2f60638cabe7547443de30565d
Dário Silva <dariooosilvaaa@gmail.com>
2022-08-25 17:01:13 -0300

    First Commit

---
+++ envio.php
@@ -0,0 +1,59 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+if (isset($_POST['enviar'])) {
+    $mail = new PHPMailer(true);
+    
+try {
+    //Server settings
+ //   $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+
+
+    $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'no-reply@andoblu.com.br';                     //SMTP username
+    $mail->Password   = '#srCwxZaF$3x32s';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Port       = 465;    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('no-reply@andoblu.com.br', 'TriboDEV');
+    $mail->addAddress('no-reply@andoblu.com.br', 'TriboDEV');     //Add a recipient
+    $mail->addReplyTo('no-reply@andoblu.com.br', 'Information');
+    $mail->isHTML(true);                                  
+    $mail->Subject = 'Mensagem enviada do Site - TriboDEV';
+
+    $body = " Mensagem enviada através do site, segue informações abaixo: <br>
+    
+    Nome:". $_POST['nome'] ." <BR>
+    E-mail: ". $_POST['email'] ." <BR>
+    Mensagem: <BR> ".  $_POST['msg'];
+
+    $mail->Body = $body; 
+
+    $enviado = $mail->send();
+    echo 'E-mail enviado com Sucesso';
+    
+} catch (Exception $e) {
+    echo "Erro ao enviar o e-mail: {$mail->ErrorInfo}";
+    }
+
+}else{
+    echo "Erro ao enviar e-mail, acesso não foi via formulário";
+ 
+}
+
+if ( $enviado ) {
+    echo "<script>
+        alert('E-mail enviado com Sucesso!');location.href='index.html';
+    </script>";
+    
+}
+?>
+
